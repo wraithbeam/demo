f0d18eab54e98f817c55586440542b14205d5054
Eremeev Vlad <eremeev.vlad.v@yandex.ru>
2022-07-12 13:25:50 +0300

    popa)

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -22,9 +22,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -71,6 +74,12 @@ public class MainController {
     private File archive = new File("C:\\Users\\vlad\\Desktop\\p.zip");
     private byte[] allBytes;
     private boolean isInFileMoreThanTwoFiles = false;
+    private final byte[] keyStartFile = {78, 70, 1, 2};
+    private final byte[] keyVariableSeparator = {78, 70, 3, 4};
+    private final byte[] bytesH = {80, 75, 1 ,2};
+    private final byte[] bytesCD = {80, 75, 5 ,6};
+    private final Charset charset = StandardCharsets.ISO_8859_1;
+
 
     /**
      * Запускает форму выбора файла, а затем отображеат ее в таблице
@@ -90,8 +99,7 @@ public class MainController {
         tableInfo.getItems().clear();
 
         filesFX.addAll(getFilesFromArchive());
-
-        //Todo Написать метод для скрытых файлов
+        filesFX.addAll(getHiddenFilesFromArchive());
 
         //Запонение таблицы + активация кнопок управления
         tableInfo.setItems(filesFX);
@@ -121,37 +129,89 @@ public class MainController {
             throw new RuntimeException(e);
         }
     }
+    private ArrayList<FileFX> getHiddenFilesFromArchive(){
+        ArrayList<FileFX> filesFX = new ArrayList<>();
+
+        for (int i = 0; i < allBytes.length - 4; i++) {
+            if ((allBytes[i] == keyStartFile[0]) & (allBytes[i + 1] == keyStartFile[1]) & (allBytes[i + 2] == keyStartFile[2]) & (allBytes[i + 3] == keyStartFile[3])) {
+
+                FileFX fileFX = new FileFX();
+
+                i+=4;
+                int length = allBytes[i++];
+                byte[] nameFile = new byte[length];
+                for (int j = 0; j < length; j++, i++){
+                    nameFile[j] = allBytes[i];
+                }
+                System.out.print(new String(nameFile, charset));
+
+                i++;
+                length = allBytes[i++];
+                byte[] lastUpdate = new byte[length];
+                for (int j = 0; j < length; j++, i++){
+                    lastUpdate[j] = allBytes[i];
+                }
+                System.out.print(new String(lastUpdate, charset));
+
+                i++;
+                length = allBytes[i++];
+                byte[] size = new byte[length];
+                for (int j = 0; j < length; j++, i++){
+                    lastUpdate[j] = allBytes[i];
+                }
+                i++;
+
+                fileFX.setName(new String(nameFile, charset));
+                fileFX.setScope("hidden");
+                fileFX.setLastUpdate(new String(lastUpdate, charset));
+                fileFX.setSize(ByteBuffer.wrap(size).getDouble());
+                filesFX.add(fileFX);
+            }
+        }
+        return filesFX;
+    }
+    private boolean notSeparator(int i) {
+        return !((allBytes[i] == keyVariableSeparator[0]) & (allBytes[i + 1] == keyVariableSeparator[1])
+                & (allBytes[i + 2] == keyVariableSeparator[2]) & (allBytes[i + 3] == keyVariableSeparator[3]));
+    }
 
 
-
-    /**
-     * Отображает форму множественного выбора файлов,
-     * затем с помощью массивов byte встраивает информацию в архив
-     */
     @FXML
     void addFiles(MouseEvent event) {
         try {
             //Запись файла в архив
             File file = selectFile(true);
+            filesFX.add(new FileFX(file, true));
             byte[] byteCodeOfUserSelectedFile = Files.readAllBytes(file.toPath());
+            int fileSeparator; //Индекс, по которому в архив будут записаны данные
 
-            int fileSeparator;
             if(isInFileMoreThanTwoFiles){
                 fileSeparator = findStartIndexForByteSequence(new byte[]{80, 75, 3, 4}, 1);
             }
             else {
                 fileSeparator = findStartIndexForByteSequence(new byte[]{80, 75, 1,2});
             }
-            formatFileCode(byteCodeOfUserSelectedFile, file);
+            byteCodeOfUserSelectedFile = addInfoIntoByteCode(byteCodeOfUserSelectedFile, file);
             writeFileIntoArchive(byteCodeOfUserSelectedFile, fileSeparator);
+            changeOffsets(byteCodeOfUserSelectedFile.length, 1);
         }
         catch (IOException e){
             throw new RuntimeException(e.getMessage());
         }
     }
 
-    private void formatFileCode(byte[] bytes, File file){
+    private byte[] addInfoIntoByteCode(byte[] bytes, File file){
+        byte[] nameFile = file.getName().getBytes(charset);
+        byte[] lastUpdate = new SimpleDateFormat("dd-MM-yyyy HH-mm-ss").format(
+                new Date(file.lastModified())).getBytes(charset);
+        byte[] size = String.valueOf(file.length()).getBytes(charset);
 
+        return joinByteArray(new byte[][]{keyStartFile,
+                new byte[]{(byte) nameFile.length}, nameFile,
+                new byte[]{(byte) lastUpdate.length}, lastUpdate,
+                new byte[]{(byte) size.length}, size,
+                new byte[]{(byte) bytes.length}, bytes
+        });
     }
 
     private void writeFileIntoArchive(byte[] fileCode, int indexOfSeparation){
@@ -188,39 +248,44 @@ public class MainController {
         return findStartIndexForByteSequence(bytes, 0);
     }
 
-    private void changeOffsets(int lengthOfFile, int startByte){
-        int[] bytesH = {80, 75, 1 ,2};
-        int[] bytesCD = {80, 75, 5 ,6};
-        for(int i = 0; i < allBytes.length - 4; i++){
-
-            if ((allBytes[i] == bytesH[0]) & (allBytes[i+1] == bytesH[1]) &(allBytes[i+2] == bytesH[2] ) & (allBytes[i+3] == bytesH[3])) {
-                i += 42;
-                int hex = lengthOfFile;
-                for (int j = 0; j < 4; j++) {
-                    hex += allBytes[i++];
-                }
-                i-=4;
-                byte[] bytes = ByteBuffer.allocate(4).putInt(hex).array();
-                for (int j = 3; j >= 0; j--, i++) {
-                    allBytes[i] = bytes[j];
+    private void changeOffsets(int lengthOfFile, int numberOccurrence){
+        int numberEntry = 0;
+        for(int i = 0; i < allBytes.length - 4; i++) {
+            if ((allBytes[i] == bytesH[0]) & (allBytes[i + 1] == bytesH[1]) & (allBytes[i + 2] == bytesH[2]) & (allBytes[i + 3] == bytesH[3])) {
+                if (numberEntry < numberOccurrence) {
+                    numberEntry++;
+                } else {
+                    i += 42;
+                    int hex = lengthOfFile;
+                    for (int j = 0; j < 4; j++) {
+                        hex += allBytes[i++];
+                    }
+                    i -= 4;
+                    byte[] bytes = ByteBuffer.allocate(4).putInt(hex).array();
+                    for (int j = 3; j >= 0; j--, i++) {
+                        allBytes[i] = bytes[j];
+                    }
                 }
             }
 
-            if ((allBytes[i] == bytesCD[0]) & (allBytes[i+1] == bytesCD[1]) &(allBytes[i+2] == bytesCD[2] ) & (allBytes[i+3] == bytesCD[3])) {
+            if ((allBytes[i] == bytesCD[0]) & (allBytes[i + 1] == bytesCD[1]) & (allBytes[i + 2] == bytesCD[2]) & (allBytes[i + 3] == bytesCD[3])) {
                 i += 16;
                 int hex = lengthOfFile;
                 for (int j = 0; j < 4; j++) {
                     hex += allBytes[i++];
                 }
-                i-=4;
+                i -= 4;
                 byte[] bytes = ByteBuffer.allocate(4).putInt(hex).array();
                 for (int j = 3; j >= 0; j--, i++) {
                     allBytes[i] = bytes[j];
                 }
+                break;
             }
         }
         save();
     }
+
+
 
 
     /**
@@ -270,25 +335,12 @@ public class MainController {
         return fileChooser.showOpenDialog(new Stage());
     }
 
-    /**
-     * Использует FileChooser
-     * @return List<File> - выбранные файлы
-     */
-    private List<File> selectFiles(){
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Select File to save");
-        fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Zip Files", "*.zip")
-        );
-        return fileChooser.showOpenMultipleDialog(new Stage());
-    }
-
     @FXML
     public void initialize() throws IOException {
-        tableColumnScope.setCellValueFactory(new PropertyValueFactory<FileFX, String>("scope"));
-        tableColumnName.setCellValueFactory(new PropertyValueFactory<FileFX, String>("name"));
-        tableColumnSize.setCellValueFactory(new PropertyValueFactory<FileFX, String>("size"));
-        tableColumnLastUpdate.setCellValueFactory(new PropertyValueFactory<FileFX, String>("lastUpdate"));
+        tableColumnScope.setCellValueFactory(new PropertyValueFactory<>("scope"));
+        tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tableColumnSize.setCellValueFactory(new PropertyValueFactory<>("size"));
+        tableColumnLastUpdate.setCellValueFactory(new PropertyValueFactory<>("lastUpdate"));
 
         //TODO удалить
         buttonAdd.setDisable(false);
@@ -309,6 +361,26 @@ public class MainController {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Соединяет все массивы в один
+     * @param bytes - двумерный массив byte[][]
+     * @return byte[] - соедененный массив
+     */
+    private byte[] joinByteArray(byte[][] bytes) {
+        int length = 0;
+        for(byte[] b : bytes){
+            length += b.length;
+        }
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(length);
+        for(byte[] b : bytes){
+            byteBuffer.put(b);
+        }
+
+        return byteBuffer.array();
+
     }
 
 }
